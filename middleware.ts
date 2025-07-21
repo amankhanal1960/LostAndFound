48915bcb22e81d409252a5933ade52e7fe9a2a8f
Aman Khanal <amankhanal1960@gmail.com>
2025-07-21 14:37:54 +0545

    feat: nothing just checking the middleware in production

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,19 @@
-import { withAuth } from "next-auth/middleware";
+// import { withAuth } from "next-auth/middleware";
 
-export default withAuth(() => {}, {
-  callbacks: {
-    authorized: ({ token }) => !!token,
-  },
-});
+// export default withAuth(() => {}, {
+//   callbacks: {
+//     authorized: ({ token }) => !!token,
+//   },
+// });
 
-export const config = {
-  matcher: [
-    "/((?!api/auth|api/users|_next|favicon.ico|auth/login|auth/register|$).*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/((?!api/auth|api/users|_next|favicon.ico|auth/login|auth/register|$).*)",
+//   ],
+// };
 
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export function middleware() {
-//   return NextResponse.next();
-// }
+export function middleware() {
+  return NextResponse.next();
+}
